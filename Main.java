e2ff890b86c5deb042a0e04fddde7b264bcef500
Burak Eker <burakeker.cs@gmail.com>
2024-09-25 16:37:41 +0300

    finalize code

--- Main.java
+++ Main.java
@@ -12,6 +12,16 @@ public class Main {
             numbers[i] = rnd.nextInt(100) + 1;
         }
     }
+
+    public static void displayDeviation() {
+        double sum = ((double)(findSum("odd") + findSum("even")));
+        double avg = sum / numbers.length;
+        for(int i : numbers) {
+            System.out.print((i - avg) + " ");
+        }
+        System.out.println();
+    }
+
     public static void getUserChoice(){
         System.out.println("---Menu Options---");
         System.out.println("1. Find the minimum of the array :");
@@ -20,70 +30,28 @@ public class Main {
         System.out.println("4. Find the sum of elements with odd indexes :");
         System.out.println("5. Find the sum of elements with even indexes :");
         System.out.println("6. Exit ");
+        System.out.print("Enter your choice: ");
     }
  
 
-    public static int findSum(int[] arr, String choice) {
+    public static int findSum(String choice) {
 
         int sum = 0;
 
         if (choice.equals("odd")) {
-            for (int i = 1; i < 100; i += 2) {
-                sum += arr[i];
+            for (int i = 1; i < numbers.length; i += 2) {
+                sum += numbers[i];
             }
         } else {
-            for (int i = 0; i < 100; i += 2) {
-                sum += arr[i];
+            for (int i = 0; i < numbers.length; i += 2) {
+                sum += numbers[i];
             }
         }
         return sum;
 
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        int size = in.nextInt();
-        numbers = new int[size];
-
-        populate();
-        int chc;
-        Scanner choice = new Scanner(System.in);
-        getUserChoice();
-        chc = choice.nextInt();
-        do{
-            getUserChoice();
-            chc = choice.nextInt();
-
-        switch(chc) {
-            
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-        }
-    
-        }while (chc != 6); 
-            
-        
-
-        
-        choice.close();
-        in.close();
-    }
-
-    private int findMinimum() {
+    public static int findMinimum() {
         int minimum = numbers[0];
         for (int i : numbers) {
             if (i < minimum) {
@@ -93,7 +61,7 @@ public class Main {
         return minimum;
     }
 
-    private int findMaximum() {
+    public static int findMaximum() {
         int maximum = numbers[0];
         for (int i : numbers) {
             if (i > maximum) {
@@ -103,13 +71,48 @@ public class Main {
         return maximum;
     }
 
-    private void displayDeviation() {
-        double sum = ((double)(findSum(numbers, "odd") + findSum(numbers, "even")));
-        double avg = sum / numbers.length;
-        for(int i : numbers) {
-            System.out.print(i - avg);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter the size of the array: ");
+        int size = in.nextInt();
+        numbers = new int[size];
+
+        populate();
+        int chc;
+
+        System.out.print("The array is: ");
+        for(int num : numbers) {
+            System.out.print(num + " ");
         }
         System.out.println();
-    }
+    
+        do{
+            getUserChoice();
+            chc = in.nextInt();
+
+        switch(chc) {
+            
+            case 1:
+                System.out.println(findMinimum());
+                break;
+            case 2:
+                System.out.println(findMaximum());
+                break;
+            case 3:
+                displayDeviation();
+                break;
+            case 4:
+                System.out.println(findSum("odd"));
+                break;
+            case 5:
+                System.out.println(findSum("even"));
+                break;
+        }
+    
+        }while (chc != 6); 
+            
+        in.close();
+    }    
 
 }
